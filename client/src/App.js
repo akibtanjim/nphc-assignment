78322156bdbaa8c73684c1b375dbb86de6fc4781
akibtanjim <akibtanjim@gmail.com>
2022-10-23 21:16:05 +0600

    tailwind.css installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   useEffect(() => {
@@ -12,21 +10,9 @@ function App() {
       .catch((e) => console.log("e", e));
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+      <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
     </div>
   );
 }
