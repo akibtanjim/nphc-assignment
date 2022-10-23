c5254de49c47910d6a8b96cb96a0564415192fc6
akibtanjim <akibtanjim@gmail.com>
2022-10-23 23:55:51 +0600

    unwanted code removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,11 @@
-import { useEffect } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import {
-  faCheckSquare,
-  faCoffee,
-  faImage,
-} from "@fortawesome/free-solid-svg-icons";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Row, Col } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Layout from "./components/Layout";
-library.add(fas, faCheckSquare, faCoffee);
+library.add(fas, faImage);
 
 function App() {
-  useEffect(() => {
-    fetch("backend/api/v1/health")
-      .then(async (r) => {
-        const res = await r.json();
-        console.log("res", res);
-      })
-      .catch((e) => console.log("e", e));
-  }, []);
   return (
     <div className="container-fluid custom-container">
       <Layout />
